ded6abb103c92dad1a25fd7774069a8ea8b4aa48
daiki.otsuka <tukadai1226@icloud.com>
2025-01-24 00:02:42 +0900

    first commit

---
+++ MyColorMemoApp/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MyColorMemoApp
+//
+//  Created by 大塚大樹 on 2025/01/22.
+//
+
+import Foundation
+import UIKit // UIに関するクラスが格納されたモジュール
+// UIViewController:UIKitのクラス
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        // このの画面が表示される際に呼び出されるメソッド
+        // 画面の表示・非表示に応じて実行されるメソッドをライフサイクルと呼ぶ
+        print("Home")
+    }
+}
